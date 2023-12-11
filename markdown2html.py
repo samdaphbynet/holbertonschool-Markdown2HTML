c8da5acb5d493a5f15d9f97b8fe37ada86f16fa0
samdaphbynet <lj8@hotmail.fr>
2023-12-11 13:21:53 +0000

    fkxed pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -8,26 +8,27 @@ import sys
 import os
 import markdown
 
+
 def convert_markdown_to_html(markdown_file, output_file):
     try:
         with open(markdown_file, 'r') as md_file:
             markdown_content = md_file.read()
             html_content = markdown.markdown(markdown_content)
-            
+
             with open(output_file, 'w') as html_file:
                 html_file.write(html_content)
-                print(f"Missing {markdown_file}", file=sys.stderr)
     except FileNotFoundError:
         sys.exit(1)
 
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
-        
+
     if not os.path.exists(sys.argv[1]):
-        print(f"Missing {sys.argv[1]}", file=sys.stderr)
+        print(f"Missing {sys.argv[1]}")
         exit(1)
-    
+
     convert_markdown_to_html(sys.argv[1], sys.argv[2])
     sys.exit(0)
