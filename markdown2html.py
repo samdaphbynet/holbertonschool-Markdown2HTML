85a8f318d7b987d7cadf25a224b4686dcebf42cb
samdaphbynet <lj8@hotmail.fr>
2023-12-11 12:48:18 +0000

    change the code

--- markdown2html.py
+++ markdown2html.py
@@ -1,34 +1,19 @@
 #!/usr/bin/python3
+"""Script that takes an argument 2 strings:
 
-import os
+First argument is the name of the Markdown file
+Second argument is the output file name"""
+
 import sys
-import markdown
-
-
-def convertMarkdownToHtml(markdown_file, output_file):
-    try:
-        with open(markdown_file, 'r') as md_file:
-            markdown_content = md_file.read()
-
-            html_content = markdown.markdown(markdown_content)
-
-            with open(output_file, 'w') as html_file:
-                html_file.write(html_content)
-
-    except FileNotFoundError:
-        print(f"Missing {markdown_file}", file=sys.stderr)
-        sys.exit(1)
-
+import os
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: ./markdown2html.py <markdown_file> <output_file>",
-              file=sys.stderr)
-        sys.exit(1)
-
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+        
     if not os.path.exists(sys.argv[1]):
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
-        sys.exit(1)
-
-    convertMarkdownToHtml(sys.argv[1], sys.argv[2])
-    sys.exit(0)
+        exit(1)
+        
+    exit(0)
