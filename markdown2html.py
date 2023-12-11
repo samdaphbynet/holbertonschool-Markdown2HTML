5fcc8ffc06e4d80052d5176dbf8983abce1678a5
samdaphbynet <lj8@hotmail.fr>
2023-12-11 13:36:53 +0000

    update

--- markdown2html.py
+++ markdown2html.py
@@ -1,33 +1,27 @@
 #!/usr/bin/python3
-"""Script that takes an argument 2 strings:
-
-First argument is the name of the Markdown file
-Second argument is the output file name"""
+"""Convert Markdown to html"""
 
 import sys
 import os
 import markdown
 
-
 def convert_markdown_to_html(markdown_file, output_file):
-    try:
-        with open(markdown_file, 'r') as md_file:
-            markdown_content = md_file.read()
-            html_content = markdown.markdown(markdown_content)
-
-            with open(output_file, 'w') as html_file:
-                html_file.write(html_content)
-    except FileNotFoundError:
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+    # Check if the Markdown file exists
+    if not os.path.exists(markdown_file):
+        sys.stderr.write("Missing " + markdown_file + "\n")
         exit(1)
 
-    if not os.path.exists(sys.argv[1]):
-        print(f"Missing {sys.argv[1]}")
+    with open(markdown_file, 'r') as md_file:
+        markdown_content = md_file.read()
+        html_content = markdown.markdown(markdown_content)
+
+        with open(output_file, 'w') as html_file:
+            html_file.write(html_content)
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
 
     convert_markdown_to_html(sys.argv[1], sys.argv[2])
