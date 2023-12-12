0234a747c8d0dad090e9391a97e6f2459cf8e0ec
samdaphbynet <lj8@hotmail.fr>
2023-12-12 09:17:26 +0000

    delete some line of documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,48 +1,31 @@
 #!/usr/bin/python3
-"""
-Markdown to HTML Converter
-convert heading
-"""
+""" Markdown to HTML Converter """
 
 from os.path import isfile
 import sys
 import markdown
 
 
-def file_exist(argv):
-    """Verify if the necessary files exist and if the correct number of arguments are provided."""
-    if len(argv) < 3:
+def markdown_to_html(markdown_file, output_file):
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-    if not isfile(argv[1]):
-        print("Missing " + argv[1], file=sys.stderr)
+    if not isfile(sys.argv[1]):
+        print("Missing " + sys.argv[1], file=sys.stderr)
         sys.exit(1)
-
-
-def markdown_to_html(markdown_file, output_file):
-    """Converts a Markdown file to HTML."""
     try:
-        # Open the Markdown file and read its content
         with open(markdown_file, 'r') as md_file:
             markdown_content = md_file.read()
-
-            # Convert Markdown to HTML with extra extensions
-            html_content = markdown.markdown(
-                markdown_content,
-                extensions=['markdown.extensions.extra']
-            )
-
-            # Open the output file and write the HTML content
+            html_content = markdown.markdown(markdown_content,
+                extensions=['markdown.extensions.extra'])
             with open(output_file, 'w') as html_file:
                 html_file.write(html_content)
 
     except FileNotFoundError:
-        # Handle the case where the specified Markdown file is not found
         print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
 
 
 if __name__ == "__main__":
-    file_exist(sys.argv)
     markdown_to_html(sys.argv[1], sys.argv[2])
