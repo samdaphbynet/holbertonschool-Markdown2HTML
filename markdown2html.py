75057a8fbd9032e3285e5a16d49fc2fcd3377d7c
samdaphbynet <lj8@hotmail.fr>
2023-12-13 20:02:06 +0000

    added comment

--- markdown2html.py
+++ markdown2html.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-""" Markdown to HTML Converter """
+"""A script converting Markdown markup language files into HTML files"""
 
 import sys
 import markdown
 
 
 def markdown_to_html(markdown_file, output_file):
+    """Convert Markdown markup file to HTML"""
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
     try:
-        # Read the file and convert it to HTML
+        """Read the file and convert it to HTML"""
         with open(markdown_file, 'r') as md_file:
             markdown_content = md_file.read()
-            html_content = markdown.markdown(markdown_content,
-                extensions=['markdown.extensions.extra'])
+            html_content = markdown.markdown(markdown_content, extensions=['markdown.extensions.extra'])
+            
+            """Write the HTML content"""
             with open(output_file, 'w') as html_file:
                 html_file.write(html_content)
 
