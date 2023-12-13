16c1789c0d77d54292b07f37d07fcd18ed8ff203
samdaphbynet <lj8@hotmail.fr>
2023-12-13 20:16:08 +0000

    parsing Headings Markdown syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -5,8 +5,9 @@ This is a module that converts a Markdown file to HTML.
 
 import sys
 import os
+import markdown
 
-def markdown2html():
+def markdown2html(markdown_file, output_file):
     """
     This function checks if the number of arguments is less than 2 and if the Markdown file exists.
     If either of these conditions is not met, it prints an error message to STDERR and exits with status code 1.
@@ -23,8 +24,19 @@ def markdown2html():
         print("Missing " + markdown_file, file=sys.stderr)
         sys.exit(1)
 
+    try:
+        with open(markdown_file, 'r') as md_file:
+            markdown_content = md_file.read()
+            html_content = markdown.markdown(markdown_content)
+            
+            with open(output_file, 'w') as html_file:
+                html_file.write(html_content)
+    except FileNotFoundError:
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
     # If the Markdown file exists, do nothing and exit with status code 0
     sys.exit(0)
 
 if __name__ == "__main__":
-    markdown2html()
+    markdown2html(sys.argv[1], sys.argv[2])
