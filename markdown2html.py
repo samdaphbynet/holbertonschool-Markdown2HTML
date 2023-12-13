e27f0269b835342429d0d2362884c5cb296d1276
samdaphbynet <lj8@hotmail.fr>
2023-12-13 20:09:15 +0000

    update the code

--- markdown2html.py
+++ markdown2html.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-"""A script converting Markdown markup language files into HTML files"""
+"""
+This is a module that converts a Markdown file to HTML.
+"""
 
 import sys
-import markdown
+import os
 
-
-def markdown_to_html(markdown_file, output_file):
-    """Convert Markdown markup file to HTML"""
+def markdown2html():
+    """
+    This function checks if the number of arguments is less than 2 and if the Markdown file exists.
+    If either of these conditions is not met, it prints an error message to STDERR and exits with status code 1.
+    Otherwise, it does nothing and exits with status code 0.
+    """
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
-    try:
-        """Read the file and convert it to HTML"""
-        with open(markdown_file, 'r') as md_file:
-            markdown_content = md_file.read()
-            html_content = markdown.markdown(markdown_content, extensions=['markdown.extensions.extra'])
-            
-            """Write the HTML content"""
-            with open(output_file, 'w') as html_file:
-                html_file.write(html_content)
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
 
-    except FileNotFoundError:
-        print(f"Missing {markdown_file}", file=sys.stderr)
+    if not os.path.exists(markdown_file):
+        print("Missing " + markdown_file, file=sys.stderr)
         sys.exit(1)
 
+    # If the Markdown file exists, do nothing and exit with status code 0
+    sys.exit(0)
 
 if __name__ == "__main__":
-    markdown_to_html(sys.argv[1], sys.argv[2])
+    markdown2html()
