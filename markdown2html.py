49ee4c7f379cd5eb447adb0f2f5434d5c850dd80
samdaphbynet <lj8@hotmail.fr>
2023-12-14 19:49:50 +0000

    add comment

--- markdown2html.py
+++ markdown2html.py
@@ -5,31 +5,30 @@ This is a module that converts a Markdown file to HTML.
 
 import sys
 import os
-import re
 import markdown
-
 
 def markdown2html():
     """
-    This function checks if the number of arguments is less than 2 and if
-    the Markdown file exists.
-    If either of these conditions is not met, it prints an error message to
-    STDERR and exits with status code 1.
-    Otherwise, it reads the Markdown file, converts the headings to HTML,
+    Converts a Markdown file to HTML.
+
+    This function checks if the number of arguments is less than 3 and if
+    the Markdown file exists. If either of these conditions is not met, it
+    prints an error message to STDERR and exits with status code 1.
+    Otherwise, it reads the Markdown file, converts the content to HTML,
     writes the result to the HTML file, and exits with status code 0.
     """
-    markdown_file = sys.argv[1]
-    html_file = sys.argv[2]
-
     # Check if we have 3 arguments
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
         sys.exit(1)
 
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
+
     # check if the file exists
     if not os.path.exists(markdown_file):
-        print("Missing " + markdown_file, file=sys.stderr)
+        print("Error: Missing " + markdown_file, file=sys.stderr)
         sys.exit(1)
 
     # Read the file and parse it
@@ -44,7 +43,6 @@ def markdown2html():
     """If the Markdown file exists and is successfully converted to HTML,
     do nothing and exit with status code 0"""
     sys.exit(0)
-
 
 if __name__ == "__main__":
     markdown2html()
