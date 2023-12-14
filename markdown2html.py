3422cc0ed8ed983c69415c02715a06a4067a13f2
samdaphbynet <lj8@hotmail.fr>
2023-12-14 18:08:55 +0000

    fixed pycodestyle

--- markdown2html.py
+++ markdown2html.py
@@ -8,17 +8,22 @@ import os
 import re
 import markdown
 
+
 def markdown2html():
     """
-    This function checks if the number of arguments is less than 2 and if the Markdown file exists.
-    If either of these conditions is not met, it prints an error message to STDERR and exits with status code 1.
-    Otherwise, it reads the Markdown file, converts the headings to HTML, writes the result to the HTML file, and exits with status code 0.
+    This function checks if the number of arguments is less than 2 and if
+    the Markdown file exists.
+    If either of these conditions is not met, it prints an error message to
+    STDERR and exits with status code 1.
+    Otherwise, it reads the Markdown file, converts the headings to HTML,
+    writes the result to the HTML file, and exits with status code 0.
     """
     markdown_file = sys.argv[1]
     html_file = sys.argv[2]
 
     if len(sys.argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
 
     if not os.path.exists(markdown_file):
@@ -32,8 +37,10 @@ def markdown2html():
         with open(html_file, 'w') as f:
             f.write(html)
 
-    # If the Markdown file exists and is successfully converted to HTML, do nothing and exit with status code 0
+    # If the Markdown file exists and is successfully converted to HTML,
+    # do nothing and exit with status code 0
     sys.exit(0)
+
 
 if __name__ == "__main__":
     markdown2html()
